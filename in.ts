b6606941a86dfdd88cafee239e87aa0c61c7f51d
aqutw <aquarian.ex@gmail.com>
2023-12-26 11:43:46 +0800

    demo "in"

---
+++ in.ts
@@ -0,0 +1,24 @@
+interface Admin {
+  name: string;
+  privileges: string[];
+}
+
+interface Employee {
+  name: string;
+  startDate: Date;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  // console.log(emp.privileges); // Error
+  // console.log(emp.startDate); // Error
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
